99f9773c351951798b8fae6250c5c70583441147
juyoung07 <yunju5090@gmail.com>
2025-05-06 19:16:39 +0900

    [feat] : 전체 task 조회 api (테스트 예정)

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import {
   HttpStatus,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './entity/task.entity';
@@ -18,19 +19,33 @@ export class TasksService {
   async create(
     project_id: number,
     createTaskDto: CreateTaskDto,
-  ): Promise<Object> {
+  ): Promise<Task> {
     try {
       const task = { createTaskDto, project_id };
 
-      await this.taskRepository.save(task);
-
-      return {
-        status: HttpStatus.CREATED,
-        message: 'Task created successfully',
-        data: task,
-      };
+      return await this.taskRepository.save(task);
     } catch (err) {
       throw new InternalServerErrorException('Failed to create Task : ', err);
+    }
+  }
+
+  async findAllTasks(): Promise<Task[]> {
+    try {
+      const tasks = await this.taskRepository.find();
+
+      if (!tasks) {
+        throw new NotFoundException('Tasks not found');
+      }
+
+      return tasks;
+    } catch (err) {
+      if (err instanceof NotFoundException) {
+        throw err;
+      }
+      throw new InternalServerErrorException(
+        'Failed to retrieve tasks : ',
+        err,
+      );
     }
   }
 }
