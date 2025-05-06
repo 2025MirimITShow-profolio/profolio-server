7c51720d35de9d470ffbd7ff4b593355d865b10f
anju0210 <s2309@e-mirim.hs.kr>
2025-05-07 00:49:10 +0900

    [feat] : 포트폴리오 저장 (로컬 저장)

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
-@Controller('portfolio')
+@Controller('api/portfolio')
 export class PortfolioController {
   constructor(private readonly portfolioService: PortfolioService) {}
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('portfolio',
+      {
+        storage: diskStorage({
+          destination: './uploads/pdfs', // 포트폴리오 저장 위치
+          filename: (req, file, cb) =>{
+            const uniqueNum = Date.now() + '-' + Math.round(Math.random() * 1e9);
+            const ext = extname(file.originalname);// 확장자
+            cb(null, file.fieldname + '-' + uniqueNum + ext);
+          }
+        }),
+      })
+  )
+  async uploadPortfolio(
+    @UploadedFile() file: Express.Multer.File,
+    @Body('project_id') projectId: number,
+){
+    return this.portfolioService.uploadPortfolio(file, projectId);
+  }
 }
--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Portfolio } from './entity/portfolio.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PortfolioService {}
+export class PortfolioService {
+  constructor(
+    @InjectRepository(Portfolio)
+    private portfolioRepository: Repository<Portfolio>
+  ) {}
+
+  async uploadPortfolio(
+    file: Express.Multer.File,
+    projectId: number
+  ): Promise<Portfolio>{
+
+    const portfolio = this.portfolioRepository.create({
+      project_id: projectId,
+      pdf_url: `/uploads/pdfs/${file.filename}`,
+    });
+
+    return this.portfolioRepository.save(portfolio);
+  }
+
+
+}
