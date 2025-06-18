6f6465047a301fc12af5c5eaac08e964ae18dedc
anju0210 <s2309@e-mirim.hs.kr>
2025-06-18 19:57:07 +0900

    [fix] : 프로젝트 생성시 불필요한 값 들어가는 에러 수정

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -23,8 +23,8 @@ export class ProjectsService {
   ): Promise<Project> {
     const newProject = this.projectRepository.create({
       ...createProjectDto,
-      team_members: user.name + "," + createProjectDto.team_members,
-      member_profile: user.profile_image + "," + createProjectDto.member_profile,
+      team_members: user.name,
+      member_profile: user.profile_image,
       user_id: user.id,
       user: user,
     });
